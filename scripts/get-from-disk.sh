a313c2e108b760075d749abd9bc9c02ed7dd5a5e
Zachary Kirtz <zach.kirtz@gmail.com>
2025-06-02 16:42:05 -0500

    scripts: Pull optimized version from/to disk.

--- scripts/get-from-disk.sh
+++ scripts/get-from-disk.sh
@@ -2,7 +2,7 @@
 
 #TODO: Allow changing these
 PDP12_TOOLS_DIR=../PDP-12/PDP-12_Tools
-TARGET_DISK_NAME=disk3.rk05
+TARGET_DISK_NAME=disk4.rk05
 TARGET_DISK_SIDE=0
 
 # Copy disk into 8tools
@@ -12,4 +12,5 @@ cp $PDP12_TOOLS_DIR/os8-disk-server/disks/$TARGET_DISK_NAME ./dhry.rk05
 $PDP12_TOOLS_DIR/8tools/os8xplode ./dhry.rk05
 
 # Copy dhrystone source
-cp ./dhry.rk05.$TARGET_DISK_SIDE/drystn.pa .
+cp ./dhry.rk05.$TARGET_DISK_SIDE/drystn.pa ./drystn.pa
+cp ./dhry.rk05.$TARGET_DISK_SIDE/drysto.pa ./drystn_opt.pa
--- scripts/pack-to-disk.sh
+++ scripts/pack-to-disk.sh
@@ -2,15 +2,16 @@
 
 #TODO: Allow changing these
 PDP12_TOOLS_DIR=../PDP-12/PDP-12_Tools
-TARGET_DISK_NAME=disk3.rk05
+TARGET_DISK_NAME=disk4.rk05
 TARGET_DISK_SIDE=0
 
 # Copy dhrystone src to disk folder
-cp ./drystn.pa ./dhry.rk05.$TARGET_DISK_SIDE
+cp ./drystn.pa ./dhry.rk05.$TARGET_DISK_SIDE/drystn.pa
+cp ./drystn_opt.pa ./dhry.rk05.$TARGET_DISK_SIDE/drysto.pa
 
 # Re-pack disk image
-$PDP12_TOOLS_DIR/8tools/os8implode $TARGET_DISK_NAME
-$PDP12_TOOLS_DIR/8tools/mkdsk $TARGET_DISK_NAME.xml+
+$PDP12_TOOLS_DIR/8tools/os8implode dhry.rk05
+$PDP12_TOOLS_DIR/8tools/mkdsk dhry.rk05.xml+
 
 # Copy disk image back into PDP-12_Tools
 cp ./dhry.rk05.new $PDP12_TOOLS_DIR/os8-disk-server/disks/$TARGET_DISK_NAME
